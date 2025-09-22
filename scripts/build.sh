9a42ca2c587c4d6e30b399d021f665716b4068f9
Araray Velho <araray@gmail.com>
2025-09-22 19:20:07 -0300

    feat - adding basic building script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,14 @@
 p_target_mode="${1:-Release}"
 
 dir_base=.
-dir_src="${dir_base}"
+dir_src="${dir_base}/"
 dir_build="${dir_base}/build"
 
 mkdir -p "${dir_build}"
 
 cd "${dir_build}"
 
-cmake -DCMAKE_BUILD_TYPE=${p_target_mode} ${dir_src}
+cmake -DCMAKE_BUILD_TYPE=${p_target_mode} "${dir_src}"
 
 cmake --build ${dir_build} -j
 
